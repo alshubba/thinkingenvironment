b199d8d38105c9496b976be3f0fa250b00cd045c
Abdulla <alshubba@keoyo.com>
2017-05-26 13:01:55 -0400

    changing email address to thinkingenvironment@gmail.com

--- thinking_environment_backend/settings.py
+++ thinking_environment_backend/settings.py
@@ -120,8 +120,8 @@ DATABASES = {
 EMAIL_USE_TLS = True
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_PASSWORD = 'owxsaeidngcowpim' #my gmail password
-EMAIL_HOST_USER = 'alshubba@gmail.com' #my gmail username
+EMAIL_HOST_PASSWORD = 'znapqlakqptfhvts' #my gmail password
+EMAIL_HOST_USER = 'thinking.environment@gmail.com' #my gmail username
 EMAIL_PORT = 587
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 
@@ -149,7 +149,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Riyadh'
 
 USE_I18N = True
 
